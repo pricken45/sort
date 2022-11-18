2f0a99ddde5140beeb5fd900ccb96a8d25b900da
Ruben <68506854+pricken45@users.noreply.github.com>
2022-11-18 13:02:06 +0000

    new version

--- quicksort.cpp
+++ quicksort.cpp
@@ -7,6 +7,61 @@
 #include <vector>
 #include <cstring>
 #include <chrono>
+#include <iostream>
+ 
+int partition(int arr[], int start, int end)
+{
+ 
+    int pivot = arr[start];
+ 
+    int count = 0;
+    for (int i = start + 1; i <= end; i++) {
+        if (arr[i] <= pivot)
+            count++;
+    }
+ 
+    // Giving pivot element its correct position
+    int pivotIndex = start + count;
+    std::swap(arr[pivotIndex], arr[start]);
+ 
+    // Sorting left and right parts of the pivot element
+    int i = start, j = end;
+ 
+    while (i < pivotIndex && j > pivotIndex) {
+ 
+        while (arr[i] <= pivot) {
+            i++;
+        }
+ 
+        while (arr[j] > pivot) {
+            j--;
+        }
+ 
+        if (i < pivotIndex && j > pivotIndex) {
+            std::swap(arr[i++], arr[j--]);
+        }
+    }
+ 
+    return pivotIndex;
+}
+ 
+void quickSort(int arr[], int start, int end)
+{
+ 
+    // base case
+    if (start >= end)
+        return;
+ 
+    // partitioning the array
+    int p = partition(arr, start, end);
+ 
+    // Sorting the left part
+    quickSort(arr, start, p - 1);
+ 
+    // Sorting the right part
+    quickSort(arr, p + 1, end);
+}
+ 
 
 std::vector<std::string> split (std::string s, std::string delimiter) {
     size_t pos_start = 0, pos_end, delim_len = delimiter.length();
@@ -27,7 +82,6 @@ std::vector<std::string> split (std::string s, std::string delimiter) {
 int main(int argc, char **argv)
 {
     auto start = std::chrono::system_clock::now();
-
     if (!(argc > 1)) {
         std::cout << "No provided input file. Closing program...\n";
         std::cout << "Usage: ./sort <input file> <output file (optional)> \n";
@@ -63,25 +117,18 @@ int main(int argc, char **argv)
     int iterations = 0;
     int len = numberVector.size() - 1;
     
-    while (swaps != 0) {
-        swaps = 0;
-        iterations++;
-        for (int i = 0; i < len; i++) {
-            //std::cout << "sorting: " << numbers[i] << ", " << numbers[i+1] << "...\n";
-            if (numbers[i] > numbers[i + 1]) {
-                swaps += 1;
-                int t1 = numbers[i];
-                int t2 = numbers[i + 1];
-                numbers[i] = t2;
-                numbers[i + 1] = t1;
-                
-            }
-        }
+    int nA[len];
+    std::copy(numbers.begin(), numbers.end(), nA);
+    quickSort(nA, 0, len - 1);
+
+    std::vector<int> dest;
+    for (int i: nA) {
+        dest.push_back(i);
     }
-    
+
     std::string numbersS = "";
 
-    for (auto l : numbers) {
+    for (auto l : dest) {
         numbersS += std::to_string(l) + "\n";
     }
     std::cout << "Sorting took " << iterations << " iterations\n";
